fb9d1c9f57dea1ba9c5451148c02a562c05195a6
Luca Duarte <luca.duarte02@gmail.com>
2024-01-27 14:09:27 -0500

    added elements to home page

--- app/pages/home.py
+++ app/pages/home.py
@@ -1,6 +1,17 @@
 from taipy.gui import Gui, navigate
+# sidebar = '''
+# <|Settings|button
+# '''
 
 home_pg = '''
 <|menu|label=Menu|lov={page_names}|on_action=on_menu|>
-Home
+#Home
+<|layout|columns = 1 1|
+    <|
+<|Placeholder|button|>
+    |>
+    <|
+<|Placeholder|button|>
+    |>
+|> 
 '''
